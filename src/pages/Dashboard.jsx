0bc3f224542d98bf8f22e1daf6acd23ee072c6ca
Krishna Chavda <krishnachavda1104@gmail.com>
2026-06-26 15:50:59 +0530

    Adjust loader margin in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -36,7 +36,7 @@ export default function Dashboard() {
     return (
     <>
         {loading ? (
-            <div className="loader-wrapper" style={{ marginTop: "70px" }}>
+            <div className="loader-wrapper" style={{ marginTop: "120px" }}>
                 <div className="spinner"></div>
                 <p>Loading Dashboard Data...</p>
             </div>
